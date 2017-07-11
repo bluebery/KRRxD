a6d3a3addd102c8b5b0016737d2ae1de3ba4adb4
Wesley Alcock <wesley.alcock@hootsuite.com>
2017-07-11 16:26:27 -0700

    fix queries

--- app/src/main/java/com/hootsuite/krrxd/persistence/UserDao.kt
+++ app/src/main/java/com/hootsuite/krrxd/persistence/UserDao.kt
@@ -16,14 +16,14 @@ interface UserDao {
      * Gets all [User]'s as a [Flowable] that emits every time the result(s) change (e.g. a record is inserted).
      * See https://medium.com/google-developers/room-rxjava-acb0cd4f3757
      */
-    @Query("select * from ${User.TABLE_NAME}")
+    @Query(value = "select * from ${User.TABLE_NAME}")
     fun getAllUsers(): Flowable<List<User>>
 
     /**
      * [Maybe] return type to be supported in the future - https://issuetracker.google.com/issues/62231019
      * For now, we have to wrap this in a fromCallable() in the consuming class.
      */
-    @Query("select * from ${User.TABLE_NAME} where ${User.COLUMN_USER_NAME} = :userName")
+    @Query(value = "select * from ${User.TABLE_NAME} where ${User.COLUMN_USER_NAME} = :userName")
     fun getUser(userName: String): User
 
     /**
@@ -36,6 +36,6 @@ interface UserDao {
     /**
      * Deletes all users and returns [Unit].
      */
-    @Query("delete from ${User.TABLE_NAME}")
+    @Query(value = "delete from ${User.TABLE_NAME}")
     fun deleteAllUsers()
 }
